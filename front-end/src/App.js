34690c8ecffdd5f3d2099c170aafedbbf2092bac
micah-sanchez <mjsanch4@gmail.com>
2023-07-01 18:27:29 -0600

    added route for /reservations/new

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
+import ReservationCreate from "./reservations";
+
 
 /**
  * Defines the root application component.
  * @returns {JSX.Element}
  */
+
+ const [reservations, setReservations] = useState(RecipeData)
+
 function App() {
   return (
     <Switch>
       <Route path="/">
         <Layout />
+      </Route>
+      <Route path="/reservations/new">
+        <ReservationCreate reservations={reservations} setReservations={setReservations }/>
       </Route>
     </Switch>
   );
