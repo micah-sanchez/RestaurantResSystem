5366a58356cf2824ec4e30eb44347590c86e5165
micah-sanchez <mjsanch4@gmail.com>
2023-07-01 18:38:39 -0600

    removed routes in app file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,17 @@
-import React, {useState} from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
-import ReservationCreate from "./reservations";
-
 
 /**
  * Defines the root application component.
  * @returns {JSX.Element}
  */
 
- const [reservations, setReservations] = useState(RecipeData)
-
 function App() {
   return (
     <Switch>
       <Route path="/">
         <Layout />
-      </Route>
-      <Route path="/reservations/new">
-        <ReservationCreate reservations={reservations} setReservations={setReservations }/>
       </Route>
     </Switch>
   );
