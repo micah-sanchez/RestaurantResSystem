d2e028e10c4da2123f5380ddf96781d1648944e3
micah-sanchez <mjsanch4@gmail.com>
2023-07-02 15:41:51 -0600

    testing list function

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,10 +1,18 @@
+const service = require("./reservations.service")
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const date = req.query.date;
+  const mobile_number = req.query.mobile_number
+  console.log("controller")
+
+  // if (mobile_number) {
+  const data = await service.list()
+  // }
+  console.log(data)
+  res.json({data});
 }
 
 module.exports = {
